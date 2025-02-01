78fc22bc76b381ce7cfcb14bc0f6c2c4773183c7
Vince Alwyn Sibay <sibay.vincealwyn@gmail.com>
2025-02-02 02:31:29 +0800

    Implement winners section

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,29 @@
-import { getLatestActiveEvent } from "@/lib/actions/event.action";
-import { Button } from "@/components/ui/button";
-import ParticipantForm from "@/components/ParticipantForm";
-import { isBefore, parseISO } from "date-fns";
+import { getLatestActiveEvent, getWinners } from "@/lib/actions/event.action";
 import { getParticipants } from "@/lib/actions/participant.action";
+import LatestEvent from "@/components/LatestEvent";
 
 export default async function Home() {
   const event = await getLatestActiveEvent();
+  console.log(event);
   if (!event.data) return null;
   const participants = await getParticipants(event.data.id);
-  console.log(event);
+  if (!participants.data) return null;
 
+  const winners = await getWinners(event.data.id);
+  console.log(winners);
+  // if (!participants.data) return null;
+  // if (!winners.data) return null;
+
+  const serverDate = new Date();
   return (
     <div>
-      <div className="">
-        <h1>{event.data.name}</h1>
-        <p>{event.data.description}</p>
-        <span>
-          {event.data.isCompleted
-            ? "Completed"
-            : isBefore(parseISO(event.data.startDate), new Date())
-            ? "Ongoing"
-            : "Upcoming"}
-        </span>
-        <Button>
-          <a href="#participantForm">Register Now</a>
-        </Button>
-      </div>
-
-      <div className="grid grid-cols-3 gap-8">
-        <div className="w-50 aspect-square bg-blue-600"></div>
-        <div className="w-50 aspect-square bg-blue-600"></div>
-        <div className="w-50 aspect-square bg-blue-600"></div>
-      </div>
-
-      <div className="lg:grid-cols-[300px_1fr]">
-        <div id="participantForm" className="">
-          {/* list of participants */}
-          <h2>Participants</h2>
-          <ul>
-            {participants.data.map((participant) => (
-              <li key={participant.id} className="flex items-center gap-2">
-                <p>{participant.name}</p>
-                <p>{participant.email}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        {/* form for participant submission */}
-        <ParticipantForm eventId={event.data.id} />
-      </div>
+      <LatestEvent
+        event={event.data}
+        participantsList={participants.data}
+        eventId={event.data.id}
+        winners={winners.data}
+        serverDate={serverDate}
+      />
     </div>
   );
 }
---
+++ components/EventWinners.tsx
@@ -0,0 +1,54 @@
+import { Participant } from "@/types/types";
+import { RefObject } from "react";
+
+export default function EventWinners({
+  winners,
+  ref,
+}: {
+  winners:
+    | {
+        winner1: Participant;
+        winner2: Participant;
+        winner3: Participant;
+      }
+    | undefined;
+
+  ref: RefObject<HTMLDivElement | null>;
+}) {
+  console.log(winners);
+  if (!winners) return null;
+  return (
+    <div className="mt-8">
+      <div
+        className="flex flex-col gap-4 mx-8 font-chakra rounded-lg border-white border-[1px] p-10 "
+        ref={ref}
+      >
+        <h2 className="text-3xl">WINNERS</h2>
+        {!winners.winner1 && !winners.winner2 && !winners.winner3 ? (
+          <h3 className="text-2xl text-center">NO WINNERS</h3>
+        ) : (
+          <div className="grid grid-cols-[1fr,_1fr,_1fr] w-full justify-between mt-8">
+            <div className="text-center">
+              <h3 className="text-3xl">
+                {winners.winner1 && winners.winner1.name}
+              </h3>
+              <span className="text-xl">🥇 1ST</span>
+            </div>
+            <div className="text-center">
+              <h3 className="text-3xl">
+                {winners.winner2 && winners.winner2.name}
+              </h3>
+              <span className="text-xl">🥈 2ND</span>
+            </div>
+            <div className="text-center">
+              <h3 className="text-3xl">
+                {winners.winner3 && winners.winner3.name}
+              </h3>
+              <span className="text-xl">🥉 3RD</span>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
